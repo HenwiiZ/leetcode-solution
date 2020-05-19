dfb8f53c39bbe082afe0cd05e518126dfdb846db
HenwiiZ <hz2620@columbia.edu>
2020-05-18 21:48:46 -0400

    54 medium matrix

---
+++ 54. Spiral Matrix.java
@@ -0,0 +1,48 @@
+class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        List<Integer> res = new LinkedList<Integer>();
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return res;
+        }
+        int up = 0;
+        int down = matrix.length - 1;
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while(up < down && left < right) {           
+            for(int i = left; i < right; i++) {
+                res.add(matrix[up][i]);
+            }
+            for(int i = up; i < down; i++) {
+                res.add(matrix[i][right]);
+            }
+            for(int i = right; i > left; i--) {
+                res.add(matrix[down][i]);
+            }
+            for(int i = down; i > up; i--) {
+                res.add(matrix[i][left]);
+            }
+            left++;
+            right--;
+            up++;
+            down--;
+        }
+        if(up == down && left == right) {
+            res.add(matrix[up][left]);
+        }
+        else if(up == down && left < right) {
+            for(int i = left; i <= right; i++) {
+                res.add(matrix[up][i]);
+            }
+            left++;
+            right--;
+        }
+        else if(up < down && left == right) {
+            for(int i = up; i <= down; i++) {
+                res.add(matrix[i][left]);
+            }
+            up++;
+            down--;
+        } 
+        return res;
+    }
+}
