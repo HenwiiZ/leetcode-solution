ec7021683dff4a174087b426cc8ff7b812bec65c
HenwiiZ <hz2620@columbia.edu>
2020-06-23 10:07:06 -0400

    222 medium bst

---
+++ 222. Count Complete Tree Nodes.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int countNodes(TreeNode root) {
+        if(root == null) return 0;
+        Queue<TreeNode> q = new LinkedList<>();
+        int sum = 0;
+        q.offer(root);
+        while(!q.isEmpty()) {
+            int len = q.size();
+            sum += len;
+            for(int i = 0; i < len; i++) {
+                TreeNode cur = q.poll();
+                if(cur.left != null) q.offer(cur.left);
+                if(cur.right != null) q.offer(cur.right);
+            }
+        }
+        return sum;
+    }
+}
